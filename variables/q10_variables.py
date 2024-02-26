39354e36e34c19d8129e8b43281c7b7a6d9a4f96
Lionel Kanyowa <lionelkanyowa@gmail.com>
2024-02-26 08:40:11 -0500

    chore: finishing up variables

---
+++ variables/q10_variables.py
@@ -0,0 +1,3 @@
+#Q10. Assume that obj already has a value of 42 when the code below starts running. Which of the subsequent statements 
+# reassign the variable? Which statements mutate the value of the object that obj references? 
+# Which statements do neither? If necessary, you can read the documentation.
---
+++ variables/q9_variables.py
@@ -0,0 +1,10 @@
+#Q9. Repeat the previous question but, this time, use augmented assignment to compute the final result, one year at a time.
+
+balance = 1000.00
+balance *= 1.05 
+balance *= 1.05 
+balance *= 1.05 
+balance *= 1.05 
+balance *= 1.05 
+
+print(balance)
