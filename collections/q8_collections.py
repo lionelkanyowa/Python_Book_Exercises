d49b46ac49db5afe26d4e82ebcc976933c81d29b
Lionel <lionelkanyowa@gmail.com>
2024-03-07 15:08:44 -0500

    chore added collectons exercises

---
+++ collections/q8_collections.py
@@ -0,0 +1,6 @@
+# Q.8 How would you print all the numbers in the following range?
+
+range(3, 17, 4)
+
+print(list(range(3, 17, 4)))
+print(tuple(range(3, 17, 4)))
---
+++ collections/q9_collections.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3, 4, [4, 5, 6]]
+another_list = list(my_list)
+
+# Are the lists assigned to my_list and another_list equal?
+# Are the lists assigned to my_list and another_list the same object?
+# Are the nested lists at index 3 of my_list and another_list equal?
+# Are the nested lists at index 3 of my_list and another_list the same object?
+
+# Don't Write any code for this exercise
